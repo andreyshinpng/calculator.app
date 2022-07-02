4dc239783d4c34e23dee2c6e0c5dcbe4e80d1a5e
andreyshinpng <andreyshinpng@yandex.kz>
2022-07-02 19:27:13 +0500

    created main.css

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculator App</title>
+</head>
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="header_logo">
+                <a href="<?php $siteAddress?>">Calculator.app</a>
+            </div>
+            <div class="github_link">
+                <a href="https://github.com/andreyshinpng/calculator.app">Github</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
